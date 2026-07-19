36b0ee3ec57fd8f23096ec4851ddc033a03d14a9
Phoenix Zerin <todofixthis@users.noreply.github.com>
2026-07-20 11:13:21 +1200

    [#100] Document use case for multiple key types 🗝️

--- test/test_gen_lookup_key.py
+++ test/test_gen_lookup_key.py
@@ -69,12 +69,11 @@ def test_unregister(customised_registry: ClassRegistry[Pokemon]) -> None:
 
 def test_use_case_aliases() -> None:
     """
-    A common use case for overriding `gen_lookup_key` is to specify some
-    aliases (e.g., for backwards-compatibility when refactoring an existing
-    registry).
+    A common use case for overriding `gen_lookup_key` is to specify aliases or
+    translations e.g. for localisation or backwards-compatibility purposes.
     """
 
-    class TestRegistry(ClassRegistry[Pokemon]):
+    class AliasRegistry(ClassRegistry[Pokemon]):
         def gen_lookup_key(self, key: typing.Hashable) -> typing.Hashable:
             """
             Simulate a scenario where we renamed the key for a class in the
@@ -86,14 +85,61 @@ def test_use_case_aliases() -> None:
 
             return key
 
-    registry = TestRegistry()
+    registry = AliasRegistry()
 
     @registry.register("flying")
     class MissingNo(Pokemon):
         pass
 
+    # MissingNo can be accessed by either key.
     assert isinstance(registry["bird"], MissingNo)
     assert isinstance(registry["flying"], MissingNo)
 
     assert "bird" in registry
     assert "flying" in registry
+
+
+def test_use_case_multiple_types() -> None:
+    """
+    Another use case for overriding `gen_lookup_key` is to allow callers to use
+    keys of different types depending on the context.
+    """
+
+    class PokemonWithID(Pokemon):
+        pokedex_id: int
+
+    class MigrationRegistry(ClassRegistry[PokemonWithID, str | int]):
+        """
+        Historically pokémon were referenced by numeric ID. We are in the
+        process of refactoring the code to reference the element instead, but
+        until the migration is complete we still have to support looking up by
+        ID.
+        """
+
+        element_from_id: dict[int, str] = {}
+
+        def _register(
+            self, key: typing.Hashable, class_: typing.Type[PokemonWithID]
+        ) -> None:
+            super()._register(key, class_)
+            self.element_from_id[class_.pokedex_id] = typing.cast(str, key)
+
+        def gen_lookup_key(self, key: int | str) -> str:
+            """
+            If the caller is providing a pokedex ID (int), look up the
+            corresponding element (str) for the lookup.
+            """
+            if isinstance(key, int):
+                return self.element_from_id[key]
+            return key
+
+    pokedex = MigrationRegistry("element")
+
+    @pokedex.register
+    class Charizard(PokemonWithID):
+        element = "fire"
+        pokedex_id = 6
+
+    # Callers can provide either the element or the legacy ID to the registry.
+    assert isinstance(pokedex["fire"], Charizard)
+    assert isinstance(pokedex[6], Charizard)
